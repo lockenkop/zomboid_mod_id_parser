04062e2a950a3bcb62edeb1101c4984b1343743e
Jonas Fenchel <locken.kop97@gmail.com>
2025-01-27 19:22:39 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+This tool uses the SteamWebApi to querry for Workshop descriptions a API key might not be needed for what we are doing here. If you get error 403 forbidden add your SteamWebApi key in a key.txt
+
+Specify you Workshop ID in main.py or in a workshop_ids.txt structured like this
+```
+2392709985;2688809268;2297098490;2169435993;3067798182;2487022075;2850135071;2988491347;2629074154;2818541726;2627877543;2616986064;2613146550;2827139313;2460154811;2200148440;2759027882;2640569820;2282429356
+```
---
+++ main.py
@@ -0,0 +1,84 @@
+from steam.webapi import WebAPI
+import re
+import httpx
+import os
+from bs4 import BeautifulSoup
+
+
+# read the Steam API key from a file, if there is no file, the key will be None
+try:
+    with open('key.txt', 'r') as f:
+        KEY = f.read().strip()
+    api = WebAPI(key=KEY)
+except FileNotFoundError:
+    print("File 'key.txt' not found, trying environment variable")
+
+try:
+    KEY = os.environ['STEAM_API_KEY']
+    api = WebAPI(key=KEY)
+except KeyError:
+    print("Environment variable 'STEAM_API_KEY' not found, defaulting to no key")
+    
+if not api:
+    api = WebAPI(key=None)
+
+def main():
+
+    # specify the workshop ids of the mods you want to get the modnames of separated by a semicolon
+    workshop_ids = ""
+    if not workshop_ids:
+        try:
+            with open('workshop_ids.txt', 'r') as f:
+                workshop_ids = f.read().strip()
+        except FileNotFoundError:
+            print("Please specify the workshop ids in the script, or create a file named 'workshop_ids.txt' and put the mod workshop ids in it")
+            return False
+
+    workshop_ids_list = workshop_ids.split(";")
+    print(f"Found {len(workshop_ids_list)} workshop ids")
+    modnames = []
+
+    for workshop_id in workshop_ids_list:
+        for method in [get_description_via_api, get_description_via_html]:
+            try:
+                description = method(api, modnames, workshop_id)
+                modname = find_modname(description)
+                if modname:
+                    modnames.append(modname)
+                    break
+                else:
+                    print(f"Failed to find modname for modid {workshop_id} via API\nmake sure the mod ")
+                    raise(KeyError)
+            except:
+                description = get_description_via_html(workshop_id)
+                modname = find_modname(description)
+                if modname:
+                    modnames.append(modname)
+                    break
+                else:
+                    print(f"Failed to find modname for modid {workshop_id} via HTML\nmake sure the mod description contains 'Mod ID: <modname>'")
+                    raise(KeyError)
+
+    print("\n\n")
+    print(';'.join(modnames))
+
+def get_description_via_api(api, modnames, modid):
+    response = api.ISteamRemoteStorage.GetPublishedFileDetails(itemcount=1, publishedfileids=[modid])
+    description = response['response']['publishedfiledetails'][0]['description']
+    return description
+    
+
+def get_description_via_html(modid):
+    response = httpx.get('https://steamcommunity.com/sharedfiles/filedetails/?id=' + modid)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    description_block = soup.find('div', class_='workshopItemDescription')
+    description = description_block.get_text(strip=True)
+    return description
+
+def find_modname(description):
+    matches = re.findall(r'Mod ID\s*:\s*([^\r\n]+)', description)
+    if matches:
+        return matches[0]
+
+if __name__ == "__main__":
+    main()
